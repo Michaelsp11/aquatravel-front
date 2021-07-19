60e2b482c9fae50d6e55cb9275f5aad5d92e04ef
Alex <alejandro.gonzalez.7e3@itb.cat>
2021-07-19 22:01:10 +0200

    cabecera

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { FaMinusCircle } from "react-icons/fa";
-import { Cabecera } from "../../../proyectos-react/ejercicio-react-17-lista-compra/src/componentes/Cabecera";
+import { Cabecera } from "./componentes/Cabecera";
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { FaMinusCircle } from "react-icons/fa";
+
 
 ReactDOM.render(
   <React.StrictMode>
