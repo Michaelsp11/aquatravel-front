881f0eca0be149262f81d8e0430755138e458dc1
Alex <alejandro.gonzalez.7e3@itb.cat>
2021-07-20 21:00:16 +0200

    formulario terminado

--- src/componentes/FormularioLogin.js
+++ src/componentes/FormularioLogin.js
@@ -3,15 +3,12 @@ export const FormularioLogin =() => {
       <div className="contenedor-formulario">
             <div className="Contenido">
 
-              <div className="form">
-                <img src="aquaTravel.png" id="icon" alt="User Icon" width="80" height="80" />
+              <div className="contenedor-imagen">
+                <img className="imagen" src="aquaTravel.png" id="icon" alt="User Icon" width="80" height="80" />
               </div>
               <form>
-                <span>Usuario</span>
                 <input type="text" id="login" className="fadeIn second" name="login" placeholder="Usuario"></input>
-                <span>Contraseña</span>
-                <input type="text" id="password" className="fadeIn third" name="password" placeholder="Contraseña"></input>
-                <span>Email</span>
+                <input type="password" id="password" className="fadeIn third" name="password" placeholder="Contraseña"></input>
                 <input type="text" id="email" className="fadeIn fourth" name="email" placeholder="Email"></input>
                 <input type="submit" className="fadeIn five" value="LogIn"></input>
               </form>
