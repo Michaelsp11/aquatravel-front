9a972dfa1f5866d61253fe5ef0acd460833b6a3d
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-07-20 21:00:59 +0200

    Navbar con btn hamburguesa + menu navegacion

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import { FaMinusCircle } from "react-icons/fa";
 import { Cabecera } from "./componentes/Cabecera";
 import { FormularioLogin } from "./componentes/FormularioLogin";
 
-
 function App() {
   return (
-    <Router>
-      <Cabecera/>
-      <Switch>
-        <Route path="/inicio" exact>
-          <Inicial />
-        </Route>
-        <Route path="/lista" exact>
-          <Lista datos={datos} />
-        </Route>
-        <Route path="/login" exact>
-          <Login error={error} loguearse={loguearse} />
-        </Route>
-        <Route path="/" exact>
-          <Redirect to="/inicio" />
-        </Route>
-        <Route path="**" exact>
-          <Redirect to="/inicio" />
-        </Route>
-      </Switch>
-    </Router>
+    <Cabecera />
+    // <Router>
+    //   <Cabecera/>
+    //   <Switch>
+    //     <Route path="/inicio" exact>
+    //       <Inicial />
+    //     </Route>
+    //     <Route path="/lista" exact>
+    //       <Lista datos={datos} />
+    //     </Route>
+    //     <Route path="/login" exact>
+    //       <Login error={error} loguearse={loguearse} />
+    //     </Route>
+    //     <Route path="/" exact>
+    //       <Redirect to="/inicio" />
+    //     </Route>
+    //     <Route path="**" exact>
+    //       <Redirect to="/inicio" />
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
- 
 }
 export default App;
