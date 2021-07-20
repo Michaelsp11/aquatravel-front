400e5b8d0263084711898c9c6b5ac91f2ade8e74
Alex <alejandro.gonzalez.7e3@itb.cat>
2021-07-20 14:02:55 +0200

    Formulario de registro

--- src/componentes/FormularioLogin.js
+++ src/componentes/FormularioLogin.js
@@ -1,21 +1,21 @@
 export const FormularioLogin =() => {
     return (
-        <div className="contenedor-formulario">
+      <div className="contenedor-formulario">
             <div className="Contenido">
 
-    <div className="fadeIn first">
-      <img src="aquaTravel.png" id="icon" alt="User Icon" width="80" height="80" />
-    </div>
-    <form>
-      <span>Usuario</span>
-      <input type="text" id="login" className="fadeIn second" name="login" placeholder="Usuario"></input>
-      <span>Contraseña</span>
-      <input type="text" id="password" className="fadeIn third" name="password" placeholder="Contraseña"></input>
-      <span>Email</span>
-      <input type="text" id="email" className="fadeIn fourth" name="email" placeholder="Email"></input>
-      <input type="submit" className="fadeIn five" value="LogIn"></input>
-    </form>
+              <div className="form">
+                <img src="aquaTravel.png" id="icon" alt="User Icon" width="80" height="80" />
+              </div>
+              <form>
+                <span>Usuario</span>
+                <input type="text" id="login" className="fadeIn second" name="login" placeholder="Usuario"></input>
+                <span>Contraseña</span>
+                <input type="text" id="password" className="fadeIn third" name="password" placeholder="Contraseña"></input>
+                <span>Email</span>
+                <input type="text" id="email" className="fadeIn fourth" name="email" placeholder="Email"></input>
+                <input type="submit" className="fadeIn five" value="LogIn"></input>
+              </form>
         </div>
-    </div>
+     </div>
     );
 };
