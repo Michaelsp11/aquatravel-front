63c4dd0804d05411427400f87befe71435a95355
Michael Salgado <michaelsalgadop11@gmail.com>
2021-07-21 10:44:21 +0200

    Mapa se ve correctamente

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,16 +1,21 @@
 export const Footer = () => {
-
-    return (
-        <>
-            <footer class="page-footer footer font-small pt-4">
-                <div class="container-fluid text-center text-md-left">
-                    <div class="row justify-content-around">
-                        <p><i class="fas fa-phone"></i> 6334221052</p>
-                        <p><i class="fab fa-instagram"></i> @aquatravel</p>
-                        <p><i class="fab fa-twitter"></i> @aquatravel</p>
-                    </div>
-                </div>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <footer className="page-footer footer font-small pt-4">
+        <div className="container-fluid text-center text-md-left">
+          <div className="row justify-content-around">
+            <p>
+              <i className="fas fa-phone"></i> 6334221052
+            </p>
+            <p>
+              <i className="fab fa-instagram"></i> @aquatravel
+            </p>
+            <p>
+              <i className="fab fa-twitter"></i> @aquatravel
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
