3e81ce18ffcc746dbb41c08781390bb01edf042b
Michael Salgado <michaelsalgadop11@gmail.com>
2021-07-21 11:01:36 +0200

    Si escribes ruta que no corresponde con ninguna pagina, te dirije a PaginaNoEncontrada

---
+++ src/pages/PaginaNoEncontrada.js
@@ -0,0 +1,11 @@
+export const PaginaNoEncontrada = () => {
+  return (
+    <main className="container">
+      <div className="row-notfound row align-items-center">
+        <div className="col-12">
+          <h1 className="text-dark text-center">Pagina no encontrada(404)</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
