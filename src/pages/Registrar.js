d5ee8e09e4a8734b760d31d13b7490ab033ea6bf
Michael Salgado <michaelsalgadop11@gmail.com>
2021-07-21 13:59:53 +0200

    Ya hay boton para registrar usuario + formulario de registro de usuario + cambio estilos formulario login

---
+++ src/pages/Registrar.js
@@ -0,0 +1,80 @@
+import { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../contextos/AuthContext";
+
+export const Registrar = () => {
+  const history = useHistory();
+  const { loguearUsuario } = useContext(AuthContext);
+  const [datosLogin, setDatosLogin] = useState({
+    usuario: "",
+    contraseña: "",
+  });
+  const [error, setError] = useState(false);
+  const setDatos = (e) => {
+    setDatosLogin({
+      ...datosLogin,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  //REACT_APP_URL_API AÑADIR .ENV
+  const enviarFormulario = async (e) => {
+    e.preventDefault();
+    const resp = await fetch(process.env.REACT_APP_URL_API + "usuarios/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datosLogin),
+    });
+    if (!resp.ok) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    const { token } = await resp.json();
+    localStorage.setItem("token", token);
+    loguearUsuario();
+    history.push("/inicio");
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center py-5">
+          <img
+            className="imagen"
+            src="aquaTravel.png"
+            id="icon"
+            alt="User Icon"
+            width="80"
+            height="80"
+          />
+        </div>
+        <form className="form-login" onSubmit={enviarFormulario}>
+          <input
+            type="text"
+            id="login"
+            className="fadeIn second"
+            name="login"
+            placeholder="Usuario"
+          ></input>
+          <input
+            type="password"
+            id="password"
+            className="fadeIn third"
+            name="password"
+            placeholder="Contraseña"
+          ></input>
+          <input
+            type="text"
+            id="email"
+            className="fadeIn fourth"
+            name="email"
+            placeholder="Email"
+          ></input>
+          <input type="submit" className="fadeIn five" value="LogIn"></input>
+        </form>
+      </div>
+    </div>
+  );
+};
