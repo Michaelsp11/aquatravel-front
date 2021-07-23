485b0320dbdca83bb84e882ef9a76e357de1b324
Michael Salgado <michaelsalgadop11@gmail.com>
2021-07-23 13:38:53 +0200

    Imagen logo redonda y grande + hamburg en mobile + fullwidth mapa

--- src/pages/Inicial.js
+++ src/pages/Inicial.js
@@ -3,26 +3,24 @@ import { useState } from "react";
 import { useEffect } from "react/cjs/react.production.min";
 
 export const Inicial = () => {
-  const [coordenadas , setCoordenadas ] = useState([41.4036299, 2.1743558]);
-navigator.geolocation.getCurrentPosition((posicion) => {
+  const [coordenadas, setCoordenadas] = useState([41.4036299, 2.1743558]);
+  navigator.geolocation.getCurrentPosition((posicion) => {
     const { latitude, longitude } = posicion.coords;
-    setCoordenadas([ latitude, longitude ]);
-});
+    setCoordenadas([latitude, longitude]);
+  });
 
-// const obtenerUbicacionActual = 
- 
+  // const obtenerUbicacionActual =
+
   return (
-    <main className="container">
-      <div className="mapa">
-         <MapContainer center={coordenadas} zoom={13}>
+    <main className="">
+      <div className="mapa w-100">
+        <MapContainer center={coordenadas} zoom={13}>
           <TileLayer
             url="https://api.mapbox.com/styles/v1/michaelsp11/ckrcg58em1hjp17o54dqcjt5i/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWljaGFlbHNwMTEiLCJhIjoiY2twaTdxdmgxMDY0dTJ3cXl4ZHRtM3prbiJ9.Mac0yVJ1kc1mI8l9ToexvQ"
             attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
           />
           <Marker position={coordenadas}>
-            <Popup>
-              Estas Aqui! o No WhoKnow!
-             </Popup>
+            <Popup>Estas Aqui! o No WhoKnow!</Popup>
           </Marker>
         </MapContainer>
       </div>
