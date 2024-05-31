93331d5f12f70e3d16b3a3c2dfea433dcf7b8f93
FGM75 <fgonzalezm75@gmail.com>
2024-05-31 17:10:16 +0200

    sry

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ export const About = () => {
     <main className="container py-5">
       <div className="row">
         <h1 className="titulo titulo-about col-12 text-center mb-4 mb-sm-0">
-          Sobre nosotros
+          Sobre mi
         </h1>
         <div className="col-12 text-center mt-md-4">
           <h2 className="titulo">Historia</h2>
@@ -19,59 +19,29 @@ export const About = () => {
         </div>
         <div className="col-12 col-md-8 mt-sm-2">
           <p>
-            AquaTravel fundada en 2021, en un bootcamp de javaScript, ha pasado
+            AquaShare fundada en 2024, como proyecto final de DAM, ha pasado
             de ser una idea pequeña a una página que cumplirá con la misión de
             descubrir el mundo de forma más fácil para todos.
           </p>
           <p>
-            AquaTravel apuesta por la tecnologia para viajar a lugares por
+            AquaShare apuesta por la tecnologia para viajar a lugares por
             descubrir. Conectando a millores de viajeros con experiencias
             únicas, y diversas opiniones para crear una comunidad de
             exploradores.
           </p>
         </div>
         <div className="col-12 text-center mt-md-4">
-          <h2 className="titulo">Fundadores</h2>
+          <h2 className="titulo">Fundador</h2>
         </div>
-        <div className="col-12">
-          <ul className="row list-unstyled">
-            <li className="col-12 col-6 col-md-4 text-center">
-              <div className="fundador shadow-sm">
-                <img
-                  src="./fotoxavi.jpg"
-                  alt="Imagen del fundador Xavier"
-                  width="200px"
-                  height="271px"
-                />
-              </div>
-            </li>
-            <li className="col-12 col-6 col-md-4 text-center">
-              <div className="fundador shadow-sm">
+        <div className="col-12 ">
+          <ul className="row list-unstyled d-flex justify-content-center align-items-center">
+            <li className="col-12 col-6 col-md-4 text-center d-flex justify-content-center align-items-center">
+              <div className="fundador shadow-sm ">
                 <img
                   src="./fotofelipe.jpg"
                   alt="Imagen del fundador Felipe"
                   width="200px"
                   height="257px"
-                />
-              </div>
-            </li>
-            <li className="col-12 col-sm-6 col-md-4 text-center">
-              <div className="fundador shadow-sm">
-                <img
-                  src="./fotoalex.jpg"
-                  alt="Imagen del fundador Alex"
-                  width="200px"
-                  height="184px"
-                />
-              </div>
-            </li>
-            <li className="col-12 col-sm-6 col-md-4 text-center">
-              <div className="fundador shadow-sm">
-                <img
-                  src="./fotomichael.jpg"
-                  alt="Imagen del fundador Michael"
-                  width="200px"
-                  height="197px"
                 />
               </div>
             </li>
